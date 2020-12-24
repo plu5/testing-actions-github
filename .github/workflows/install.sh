c910ec95cb5eea26d81ae7c49dcc312a2b0fa744
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-24 00:57:50 -0600

    Added the temporal input for test purpose; Commented the package update.

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 echo "*** INIT: Install script"
-sudo apt update
-sudo apt install -y pandoc
+#sudo apt update
+#sudo apt install -y pandoc
 echo "*** END: Install script"
 echo "Hello World!" > hello.txt
 echo "# ----------------------------------------"
