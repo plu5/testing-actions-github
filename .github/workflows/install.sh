06a642d4f0edfec3d1d60515cd77ab6b9567842f
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-23 22:53:01 -0600

    Cleaned the action script; Added a hello world text for test.

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -4,3 +4,5 @@ echo "*** INIT: Install script"
 sudo apt update
 sudo apt install -y pandoc
 echo "*** END: Install script"
+echo "Hello World!" > hello.txt
+echo "# ----------------------------------------"
